72794860ac21d6bab84f413fb74e08f4d7652bd0
Joshua Edgerton <joshuaxedgerton@gmail.com>
2019-12-16 11:45:21 -0600

    initial small notes added

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,8 @@ module.exports = {
       filename: './database/auth.db3',
     },
     pool: {
+      // SQlite will NOT enforce foreign keys
+      // add this pool config to turn them on
       afterCreate: (conn, done) => {
         conn.run('PRAGMA foreign_keys = ON', done);
       },
